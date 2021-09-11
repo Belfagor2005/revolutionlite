bc0a73d1ce6ae888c2ce26204312c0d3a245f945
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2021-09-11 16:26:02 +0200

    Update __init__.py

--- usr/lib/enigma2/python/Plugins/Extensions/revolution/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/revolution/__init__.py
@@ -2,28 +2,20 @@
 # -*- coding: utf-8 -*-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-import gettext, base64
+import gettext
+import os
 from os import environ as os_environ
-plugin_path = '/usr/lib/enigma2/python/Plugins/Extensions/revolution/'
 PluginLanguageDomain = 'revolution'
 PluginLanguagePath = 'Extensions/revolution/res/locale'
-# UrlSvr = 'aHR0cDov+L3BhdGJ+1d2ViLmN+vbS9pcH+R2Lw=='
-# UrlSvr = UrlSvr.replace('+', '')
-# UrlLst = base64.b64decode(UrlSvr)
-try:
-    from enigma import eMediaDatabase
-    isDreamOS = True
-except:
-    isDreamOS = False
 
 def localeInit():
-    if isDreamOS:
+    if os.path.exists('/var/lib/dpkg/status'):
         lang = language.getLanguage()[:2]
         os_environ['LANGUAGE'] = lang
     gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
-if isDreamOS:
+if os.path.exists('/var/lib/dpkg/status'):
     _ = lambda txt: gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
     localeInit()
     language.addCallback(localeInit)
