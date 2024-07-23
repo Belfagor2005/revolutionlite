e2132eae67afedbc3ef8bc8b18d9666e1dceba3b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-24 00:01:21 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/revolution/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/revolution/plugin.py
@@ -3745,9 +3745,31 @@ def main(session, **kwargs):
         traceback.print_exc()
 
 
+# def menu(menuid, **kwargs):
+    # if menuid == 'mainmenu':
+        # return [(desc_plug, main, title_plug, 44)]
+    # else:
+        # return []
+
+
+# def menu(menuid, **kwargs):
+    # if menuid == 'mainmenu':
+        # from Tools.BoundFunction import boundFunction
+        # return [(desc_plug,
+                 # boundFunction(main, showExtentionMenuOption=True),
+                 # title_plug,
+                 # 55)]
+    # else:
+        # return []
+
+
 def menu(menuid, **kwargs):
     if menuid == 'mainmenu':
-        return [(desc_plug, main, title_plug, 44)]
+        from Tools.BoundFunction import boundFunction
+        return [(desc_plug,
+                 boundFunction(main, showExtentionMenuOption=True),
+                 title_plug,
+                 55)]
     else:
         return []
 
